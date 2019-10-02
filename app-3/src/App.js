aa1d06a598cd288aeccaa3382d45c08a1d2b6b59
Fei Williams <55406776+flyfei1999@users.noreply.github.com>
2019-10-01 20:01:27 -0700

    Updated

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -3,16 +3,32 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      filterString: "",
+      name: ["Fei", "Dewei", "Zaiquan", "Lianchen"]
+    };
+  }
+
+  handleChange(filter) {
+    this.setState({ filterString: filter });
+  }
+
   render() {
+    console.log(this.state);
+    let nameToDisplay = this.state.name
+      .filter((element, index) => {
+        return element.includes(this.state.filterString);
+      })
+      .map((element, index) => {
+        return <h2 key={index}>{element}</h2>;
+      });
+    console.log(nameToDisplay);
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input onChange={e => this.handleChange(e.target.value)} type="text" />
+        {nameToDisplay}
       </div>
     );
   }
